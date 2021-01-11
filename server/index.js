e9e860009415a348413cc245d592ee99900908a0
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-11 15:43:25 -0700

    Finished step 5

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
+const ctrl = require('./products_controller');
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
 const app = express();
 
@@ -14,6 +15,11 @@ massive({
     console.log('db connected');
 }).catch(err => console.log(err));
 
-
+//endpoints
+app.get('/api/products', ctrl.getAll);
+app.get('/api/products/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.post('/api/products', ctrl.create);
+app.delete('/api/products/:id', ctrl.delete);
 
 app.listen(SERVER_PORT, () => console.log(`Server is running on port ${SERVER_PORT}`));
