9878fa8feaee2cd45392a5a3da35a9200fac12f1
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-11 14:07:54 -0700

    Finished step 1

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const {SERVER_PORT} = process.env;
+const app = express();
+
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => console.log(`Server is running on ${SERVER_PORT}`));
